8b66df460d06e852ba9162ab66e70bbde80b9563
Yi Ou <ouyi@gmx.de>
2018-06-03 02:04:09 +0200

    Play with themes and styles

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
-import { HomePage } from '../home/home';
 
 /**
  * Generated class for the SettingsPage page.
