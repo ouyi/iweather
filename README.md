ddc086c94ef90006b6737b8d6558a36b1cfa3429
Yi Ou <ouyi@gmx.de>
2018-06-03 11:50:44 +0200

    Get ionic build --prod working

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-ionic start iweather tabs --no-git
-ionic serve
 
-ionic g page settings
-app.module.ts
-tabs.ts
-tabs.html
+    ionic start iweather tabs --no-git
+    ionic serve
 
+    ionic g page settings
+    app.module.ts
+    tabs.ts
+    tabs.html
+
+    ionic cordova platform add browser
+    ionic build --prod
+    platforms/browser/www/
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 
 /**
@@ -9,7 +9,6 @@ import { Storage } from '@ionic/storage';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html',
