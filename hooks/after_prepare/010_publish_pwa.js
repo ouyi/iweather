79fddbac6ae4ba06a892d74a6f0e421046f343ac
Yi Ou <ouyi@gmx.de>
2018-06-04 22:12:20 +0200

    Comment out cordova.js and enable service worker

--- hooks/after_prepare/010_publish_pwa.js
+++ hooks/after_prepare/010_publish_pwa.js
@@ -9,19 +9,7 @@ var path = require('path');
 const src = 'www';
 const dest = 'docs';
 
+console.log(`Copying ${src}/* to ${dest}/ ...`);
 shell(`mkdir -p ${dest}`);
 shell(`cp -r ${src}/* ${dest}`);
-
-const indexFileSrc = `${src}/index.html`;
-const indexFileDest = `${dest}/index.html`;
-
-fs.readFile(indexFileSrc, 'utf8', function (err, data) {
-  if (err) {
-    return console.log(err);
-  }
-  var result = data.replace(/<script src="cordova.js"><\/script>/g, match => `<!--${match}-->`);
-
-  fs.writeFile(indexFileDest, result, 'utf8', function (err) {
-    if (err) return console.log(err);
-  });
-});
+console.log('Done.');
